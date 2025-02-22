eafc76bff78342b8a5b8d9df14608ece3379f2d1
Abhigyan Sanyal <abhigyansanyal@Abhigyans-MacBook-Air.local>
2025-02-22 16:53:04 +0530

    updating

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "weather" {
-  bucket = "weather-app"
+  bucket = "weatherapp97346"
 }
 
 resource "aws_iam_role" "lambda_exec" {
@@ -32,8 +32,8 @@ resource "aws_iam_policy_attachment" "lambda_basic_execution" {
 }
 
 resource "aws_lambda_function" "weather-app" {
-  function_name    = "servweather-app"
-  filename        = "../lambda/lambda.zip"
+  function_name    = "serverless-weather-app"
+  filename        = "../lambda/Lambda.zip"
   source_code_hash = filebase64sha256("../lambda/Lambda.zip")
   role            = aws_iam_role.lambda_exec.arn
   handler         = "index.handler"
