379cf589835d9053e1607656b3d72b13156e1d27
Abhigyan Sanyal <abhigyansanyal@Abhigyans-MacBook-Air.local>
2025-02-22 16:41:37 +0530

    updating

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,8 +8,31 @@ resource "aws_s3_bucket" "weather" {
   bucket = "weather-app"
 }
 
+resource "aws_iam_role" "lambda_exec" {
+  name = "lambda-exec-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy_attachment" "lambda_basic_execution" {
+  name       = "lambda-basic-execution"
+  roles      = [aws_iam_role.lambda_exec.name]
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
 resource "aws_lambda_function" "weather-app" {
-  function_name    = "serverless-weather-app"
+  function_name    = "servweather-app"
   filename        = "../lambda/lambda.zip"
   source_code_hash = filebase64sha256("../lambda/lambda.zip")
   role            = aws_iam_role.lambda_exec.arn
